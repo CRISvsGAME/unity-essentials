09ad04e71d0a79f39d3809e6cfd3a4aa0c7e1ac7
CRISvsGAME <cris@crisvsgame.com>
2025-09-09 20:00:00 +0000

    Unity Essentials 0.0.14
    Add Movement

--- Assets/Scripts/Input/InputActions.cs
+++ Assets/Scripts/Input/InputActions.cs
@@ -7,16 +7,21 @@ public class InputActions : MonoBehaviour
     private InputActionAsset _inputActions;
     private InputActionMap _playerActionMap;
     private InputAction _lookAction;
+    private InputAction _moveAction;
 
     [SerializeField]
     private float _sensitivity = 100f;
     private float _xRotation = 0f;
     private float _yRotation = 0f;
 
+    [SerializeField]
+    private float _moveSpeed = 10f;
+
     private void Awake()
     {
         _playerActionMap = _inputActions.FindActionMap("Player");
         _lookAction = _playerActionMap.FindAction("Look");
+        _moveAction = _playerActionMap.FindAction("Move");
     }
 
     private void OnEnable()
@@ -36,5 +41,9 @@ public class InputActions : MonoBehaviour
         _yRotation += lookDelta.x * _sensitivity * Time.deltaTime;
         _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
         transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0f);
+
+        Vector2 movement = _moveAction.ReadValue<Vector2>();
+        Vector3 direction = transform.forward * movement.y + transform.right * movement.x;
+        transform.position += _moveSpeed * Time.deltaTime * direction;
     }
 }
