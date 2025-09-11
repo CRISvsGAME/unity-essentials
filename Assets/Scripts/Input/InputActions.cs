d0dcca3c2c5e7c6fbf69bb68bc1f99bd778ffbba
CRISvsGAME <cris@crisvsgame.com>
2025-09-11 20:00:00 +0000

    Unity Essentials 0.0.16
    Add Movement Clamping

--- Assets/Scripts/Input/InputActions.cs
+++ Assets/Scripts/Input/InputActions.cs
@@ -18,6 +18,15 @@ public class InputActions : MonoBehaviour
     [SerializeField]
     private float _moveSpeed = 10f;
 
+    [SerializeField]
+    private bool _movementClamp = false;
+
+    [SerializeField]
+    private Vector3 _minBounds = new(-15f, 1f, -15f);
+
+    [SerializeField]
+    private Vector3 _maxBounds = new(15f, 30f, 15f);
+
     private void Awake()
     {
         _playerActionMap = _inputActions.FindActionMap("Player");
@@ -44,11 +53,25 @@ public class InputActions : MonoBehaviour
         _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
         transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0f);
 
-        Vector2 movement = _moveAction.ReadValue<Vector2>();
-        Vector3 direction = transform.forward * movement.y + transform.right * movement.x;
-        transform.position += _moveSpeed * Time.deltaTime * direction;
+        Vector2 m = _moveAction.ReadValue<Vector2>();
+        float v = _verticalAction.ReadValue<float>();
 
-        float vertical = _verticalAction.ReadValue<float>();
-        transform.position += _moveSpeed * Time.deltaTime * vertical * transform.up;
+        Vector3 direction = transform.forward * m.y + transform.right * m.x + transform.up * v;
+
+        if (direction.sqrMagnitude > 1f)
+        {
+            direction.Normalize();
+        }
+
+        Vector3 position = _moveSpeed * Time.deltaTime * direction + transform.position;
+
+        if (_movementClamp)
+        {
+            position.x = Mathf.Clamp(position.x, _minBounds.x, _maxBounds.x);
+            position.y = Mathf.Clamp(position.y, _minBounds.y, _maxBounds.y);
+            position.z = Mathf.Clamp(position.z, _minBounds.z, _maxBounds.z);
+        }
+
+        transform.position = position;
     }
 }
