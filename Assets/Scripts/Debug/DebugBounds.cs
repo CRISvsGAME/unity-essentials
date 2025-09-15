1154e96cc18ee975df85cd09f4fef55b732dedb9
CRISvsGAME <cris@crisvsgame.com>
2025-09-15 20:00:00 +0000

    Unity Essentials 0.0.20
    Add Debug Bounds Script

---
+++ Assets/Scripts/Debug/DebugBounds.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[ExecuteAlways]
+public class DebugBounds : MonoBehaviour
+{
+    private Renderer _renderer;
+
+    private void Awake()
+    {
+        _renderer = GetComponentInChildren<Renderer>();
+    }
+
+    private void Update()
+    {
+        Debug.Log(
+            $"{name} Bounds | X: {_renderer.bounds.size.x:F10} Y: {_renderer.bounds.size.y:F10} Z: {_renderer.bounds.size.z:F10}"
+        );
+    }
+}
