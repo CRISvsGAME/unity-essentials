4a1639dab853a432fc4065bb65b2c909bc9bc129
CRISvsGAME <cris@crisvsgame.com>
2025-10-24 20:00:00 +0000

    Unity Essentials 0.1.17
    Implement Interactible Interface

--- Assets/Scripts/Raycast/CameraRaycast.cs
+++ Assets/Scripts/Raycast/CameraRaycast.cs
@@ -5,13 +5,27 @@ public class CameraRaycast : MonoBehaviour
     [SerializeField]
     private float _rayDistance = 5f;
 
+    private IInteractable _currentInteractable;
+
     void Update()
     {
         Ray ray = new(transform.position, transform.forward);
 
         if (Physics.Raycast(ray, out RaycastHit hitInfo, _rayDistance))
         {
-            Debug.Log("Hit Object: " + hitInfo.collider.gameObject.name);
+            IInteractable interactable = hitInfo.collider.GetComponentInParent<IInteractable>();
+
+            if (interactable != _currentInteractable)
+            {
+                _currentInteractable?.OnLookExit();
+                _currentInteractable = interactable;
+                _currentInteractable?.OnLookEnter();
+            }
+        }
+        else
+        {
+            _currentInteractable?.OnLookExit();
+            _currentInteractable = null;
         }
     }
 }
